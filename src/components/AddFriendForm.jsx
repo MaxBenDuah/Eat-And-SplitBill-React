2cad4ad90b74b984c78a4625d69818da36cf284c
MaxBenDuah <maxwellben728@gmail.com>
2024-05-04 20:14:50 +0100

    Update AddFriendForm Component by putting date logic outside component

--- src/components/AddFriendForm.jsx
+++ src/components/AddFriendForm.jsx
@@ -1,25 +1,29 @@
 import React, { useState } from "react";
 import Button from "./Button";
 
-const AddFriendForm = function ({onAddFriend}) {
+const now = new Date();
+
+const optionTime = {
+  hour: "numeric",
+  minute: "numeric",
+};
+
+const optionDate = {
+  month: "short",
+  year: "numeric",
+  day: "numeric",
+};
+
+const time = new Intl.DateTimeFormat(navigator.language, optionTime).format(
+  now
+);
+const date = new Intl.DateTimeFormat(navigator.language, optionDate).format(
+  now
+);
+
+const AddFriendForm = function ({ onAddFriend }) {
   const [name, setName] = useState("");
   const [img, setImg] = useState("");
-
-  const now = new Date();
-
-  const optionTime = {
-    hour: "numeric",
-    minute: "numeric"
-  };
-
-  const optionDate = {
-    month: "short",
-    year: "numeric",
-    day: "numeric"
-  };
-
-  const time = new Intl.DateTimeFormat(navigator.language, optionTime).format(now);
-  const date = new Intl.DateTimeFormat(navigator.language, optionDate).format(now);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -32,29 +36,37 @@ const AddFriendForm = function ({onAddFriend}) {
       time,
       date,
       id: Date.now(),
-      balance: Math.floor(Math.random() * 20)
+      balance: Math.floor(Math.random() * 20),
     };
 
     onAddFriend(addFriend);
 
     setName("");
     setImg("");
-  }
+  };
 
   return (
     <form onSubmit={handleSubmit} className="add--friend-form">
       <div className="input--label">
         <label>Enter Friend name</label>
-        <input type="text" value={name} onChange={e => setName(e.target.value)} />
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
       </div>
 
       <div className="input--label">
         <label>Image URL</label>
-        <input type="text" value={img} onChange={e => setImg(e.target.value)} />
+        <input
+          type="text"
+          value={img}
+          onChange={(e) => setImg(e.target.value)}
+        />
       </div>
       <Button className="btn btn--right input--btn">Add</Button>
     </form>
   );
-}
+};
 
 export default AddFriendForm;
